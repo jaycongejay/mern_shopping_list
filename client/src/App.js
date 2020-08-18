c69b7e3129222deef7ad1e69753f54ddd5ea3f16
Jaehyung Lee <jhcongejh@gmail.com>
2020-08-18 06:41:35 -0400

    implement Redux on Front-end

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 
 class App extends Component {
 
   render(){
 
     return (
-      <div className="App">
-        <AppNavbar/>
-        <ShoppingList/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar/>
+          <Container>
+            <ItemModal/>
+            <ShoppingList/>
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
