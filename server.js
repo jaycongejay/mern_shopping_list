243656e1c1869713c6ffc6e110b92854842ac831
Jaehyung Lee <jhcongejh@gmail.com>
2020-08-18 16:12:29 -0400

    Hosting_1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 // const cors = require('cors');
 // const config = require('config');
 
@@ -41,6 +42,17 @@ app.use('/api/items', items);
 // app.get('/', (req, res) => {
 //     res.send('We are on home');
 // });
+
+
+// For PRODUCTION mode (when production mode, automatically access the npm run build file)
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'clinet', 'build', 'index.html'));
+    })
+}
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
